a0bbcf85434a45e90314eeece8ce1a2a3f6b1a53
MomusWinner <areg06.06@mail.ru>
2024-10-16 01:25:02 +0300

    implement dialog system

---
+++ dialog.js
@@ -0,0 +1,44 @@
+import { Person } from "./characters/person.js";
+import { player } from "./characters/player.js";
+import { Event } from "./event.js";
+import {printLine} from "./utils.js"
+
+export class PhraseEvent extends Event{
+    /**
+     * 
+     * @param {Person} person 
+     * @param {string} text 
+     */
+    constructor(person, text){
+        super()
+        this.person = person
+        this.text = text
+    }
+
+    async start(){
+        console.log(typeof(this.person))
+        if(this.person === player)
+            console.log("★ " + this.person.name)
+        else
+            console.log("✩ " + this.person.name)
+        await printLine(this.text)
+    }
+}
+
+export class DialogEvent extends Event{
+    /**
+     * 
+     * @param {Array<Event>} dialogEvents 
+     */
+    constructor(dialogEvents){
+        super()
+        this.dialogEvents = dialogEvents
+    }
+
+    async start(){
+        for (let i = 0; i < this.dialogEvents.length; i++) {
+            await this.dialogEvents[i].start()
+        }
+    }
+}
+
--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 import { player } from './characters/player.js';
 import {Location} from './location.js';
 import {Choice, ChoiceEvent} from './event.js';
-
+import {DialogEvent, PhraseEvent} from './dialog.js'
+import { Person } from './characters/person.js';
 
 let choice1 = new Choice("choice1", () => {console.log("response  1");})
 let choice_event = new ChoiceEvent([choice1])
 
 let forest = new Location("Лес", "Лес..", choice_event)
 
-await forest.start()
+
+let ashot = new Person("Ashot", 1000)
+
+
+let phrases = [
+  new PhraseEvent(player, "Hello"),
+  new PhraseEvent(ashot, "a.."),
+  new PhraseEvent(player, "a"),
+  new PhraseEvent(ashot, "o.."),
+]
+
+let dial = new DialogEvent(phrases)
+
+await phrases[0].start()
+await phrases[1].start()
+// await dial.start()
+// await forest.start()
