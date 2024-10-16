9ce5d4edcbd3dee3087bb9977c9da799dcce1dd7
MomusWinner <areg06.06@mail.ru>
2024-10-16 09:23:05 +0300

    fix choice event

--- event.js
+++ event.js
@@ -40,7 +40,7 @@ export class ChoiceEvent extends Event{
         for (let i = 0; i < this.choices.length; i++) {
             /** @type {Choice} */
             const choice = this.choices[i];
-            console.log(i + ". " + choice.text)
+            console.log((i + 1) + ". " + choice.text)
         }
     }
 }
