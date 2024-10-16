b669696c24c81d59bf37ad7e082b2e334a671356
spl3g <spleefer6@yandex.ru>
2024-10-16 09:40:25 +0300

    add player attacking caps

--- characters/player.js
+++ characters/player.js
@@ -1,9 +1,31 @@
+import { Choice, ChoiceEvent } from "../event.js"
+import { printLine } from "../utils.js"
+import { Attack } from "./enemy.js"
 import {Person} from "./person.js"
 
 class Player extends Person{ 
     constructor(name, health){
         super(name, health)
         this.inventory = []
+        this.attacks = [
+            new Attack("Удар молотком", 50),
+            new Attack("Удар головой", 100)
+        ]
+    }
+
+    async getAttack() {
+        await printLine(`Выберите атаку:`)
+        let chosenAttack
+
+        const choices = this.attacks.map((attack) => {
+            const choiceString = `${attack.name} -- ${attack.damage} урона`
+            return new Choice(choiceString, () => {
+                chosenAttack = attack
+            })
+        })
+
+        await new ChoiceEvent(choices, "Выберите атаку:").start()
+        return chosenAttack
     }
 }
 
