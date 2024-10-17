bfc9c7b00d0994e537a573761e1291673ead7abd
MomusWinner <areg06.06@mail.ru>
2024-10-17 23:20:12 +0300

    update main

--- main.js
+++ main.js
@@ -2,9 +2,4 @@ import {LocationEvent} from './events/location.js';
 import { barn } from './locations/barn.js'
 import { village } from './locations/village.js'
 
-// ------------------------
-let forest = new LocationEvent("Лес", "Вонючий лес")
-
-// ------------------------
-
-await village.start()
+await barn.start()
